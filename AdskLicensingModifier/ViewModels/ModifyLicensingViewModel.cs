def6e8f94117e724c5017e405c9adb7864ec04ca
Tobias Wiesendanger <tobias.wiesendanger@gmail.com>
2023-03-08 23:20:20 +0100

    feat: changed to now use enums for listview feat: added creating of cmd command draft: started with generate and run command

--- AdskLicensingModifier/ViewModels/ModifyLicensingViewModel.cs
+++ AdskLicensingModifier/ViewModels/ModifyLicensingViewModel.cs
@@ -1,8 +1,9 @@
-﻿using Windows.UI;
+﻿using Windows.ApplicationModel.DataTransfer;
+using Windows.UI;
 using AdskLicensingModifier.Helpers;
 using CommunityToolkit.Mvvm.ComponentModel;
 using AdskLicensingModifier.Contracts.Services;
-using Newtonsoft.Json.Linq;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AdskLicensingModifier.ViewModels;
 
@@ -14,14 +15,24 @@ public partial class ModifyLicensingViewModel : ObservableRecipient
         get;
     }
 
+    private const string LICENSE_HELPER_EXE =
+        @"C:\Program Files (x86)\Common Files\Autodesk Shared\AdskLicensing\Current\helper\AdskLicensingInstHelper.exe";
     [ObservableProperty] private string? searchText;
-    [ObservableProperty] private ListViewItem? selectedLicense;
+    [ObservableProperty] private string? result;
+    [ObservableProperty] private string? serverNames;
+    [ObservableProperty] private LicenseType selectedLicenseType = LicenseType.Reset;
+    [ObservableProperty] private ServerType selectedServerType;
+    [ObservableProperty] private List<ServerType> serverTypes;
+    [ObservableProperty] private List<LicenseType> licenseTypes;
     [ObservableProperty] private bool uiIsenabled;
+    [ObservableProperty] private bool serverTypeIsEnabled;
     [ObservableProperty] private Dictionary<string, string> adskProducts;
     [ObservableProperty] private Dictionary<string, string> filteredAdskProducts;
     [ObservableProperty] private Dictionary<string, string> filteredYearAdskProducts;
-    [ObservableProperty] private string selectedProduct;
+    [ObservableProperty] private KeyValuePair<string, string> selectedProduct;
     [ObservableProperty] private string selectedYear = "2023";
+    private string _productFeatureCode = "2020.0.0.F";
+
     public ModifyLicensingViewModel(IGenericMessageDialogService messageDialogService)
     {
         _messageDialogService = messageDialogService;
@@ -39,6 +50,10 @@ public partial class ModifyLicensingViewModel : ObservableRecipient
         FilteredYearAdskProducts = string.IsNullOrEmpty(SelectedYear) ? AdskProducts : AdskProducts.Where(x => x.Key.Contains(SelectedYear, StringComparison.OrdinalIgnoreCase))
         .ToDictionary(pair => pair.Key, pair => pair.Value);
         FilteredAdskProducts = FilteredYearAdskProducts;
+
+
+        ServerTypes = new List<ServerType>(Enum.GetValues(typeof(ServerType)).Cast<ServerType>());
+        LicenseTypes = new List<LicenseType>(Enum.GetValues(typeof(LicenseType)).Cast<LicenseType>());
     }
 
     partial void OnSelectedYearChanged(string value)
@@ -46,6 +61,16 @@ public partial class ModifyLicensingViewModel : ObservableRecipient
         FilteredYearAdskProducts = string.IsNullOrEmpty(value) ? AdskProducts : AdskProducts.Where(x => x.Key.Contains(value, StringComparison.OrdinalIgnoreCase))
             .ToDictionary(pair => pair.Key, pair => pair.Value); ;
         FilteredAdskProducts = FilteredYearAdskProducts;
+
+        _productFeatureCode = value switch
+        {
+            "2020" => "2020.0.0.F",
+            "2021" => "2021.0.0.F",
+            "2022" => "2022.0.0.F",
+            "2023" => "2023.0.0.F",
+            "2024" => "2024.0.0.F",
+            _ => ""
+        };
         SearchText = "";
     }
 
@@ -63,8 +88,7 @@ public partial class ModifyLicensingViewModel : ObservableRecipient
     private async Task CheckPath()
     {
         var exeCheck =
-            File.Exists(
-                @"C:\Program Files (x86)\Common Files\Autodesk Shared\AdskLicensing\Current\helper\AdskLicensingInstHelper.exe");
+            File.Exists(LICENSE_HELPER_EXE);
 
         if (exeCheck == false)
         {
@@ -87,5 +111,80 @@ public partial class ModifyLicensingViewModel : ObservableRecipient
         //TODO maybe add a delay here
         FilteredAdskProducts = string.IsNullOrEmpty(value) ? FilteredYearAdskProducts : FilteredYearAdskProducts.Where(x => x.Key.Contains(value, StringComparison.OrdinalIgnoreCase))
             .ToDictionary(pair => pair.Key, pair => pair.Value);
+    }
+
+    partial void OnSelectedLicenseTypeChanged(LicenseType value)
+    {
+        switch (value)
+        {
+            case LicenseType.Network:
+                SelectedServerType = SelectedServerType = ServerType.Single;
+                ServerTypeIsEnabled = true;
+                break;
+            default:
+                SelectedServerType = SelectedServerType;
+                ServerTypeIsEnabled = false;
+                break;
+        }
+    }
+
+    [RelayCommand]
+    public async Task Generate()
+    {
+        if (string.IsNullOrWhiteSpace(SelectedProduct.Key))
+        {
+            var dialogSettings = new DialogSettings()
+            {
+                Title = "No product selected",
+                Message = $"No product was selected. Make sure you have selected one product. ",
+                Color = Color.FromArgb(255, 234, 93, 97),
+                Symbol = ((char)0xEA39).ToString(),
+            };
+            await _messageDialogService.ShowDialog(dialogSettings);
+        }
+
+        var cmdCommand = SetCmdCommand();
+        Result = cmdCommand;
+
+        var dataPackage = new DataPackage();
+        dataPackage.SetText(cmdCommand);
+        Clipboard.SetContent(dataPackage);
+    }
+
+    private string SetCmdCommand()
+    {
+        var cmdCommand = SelectedLicenseType switch
+        {
+            LicenseType.Network =>
+                $"{LICENSE_HELPER_EXE} change --prod_key {SelectedProduct.Value} --prod_ver {_productFeatureCode} --lic_method {SelectedLicenseType.ToString().ToUpper()} --lic_server_type {SelectedServerType.ToString().ToUpper()} --lic_servers {ServerNames}",
+            LicenseType.Standalone or LicenseType.User =>
+                $"{LICENSE_HELPER_EXE} change --prod_key {SelectedProduct.Value} --prod_ver {_productFeatureCode} --lic_method {SelectedLicenseType.ToString().ToUpper()}",
+            LicenseType.Reset =>
+                $"{LICENSE_HELPER_EXE} change --prod_key {SelectedProduct.Value} --prod_ver {_productFeatureCode} --lic_method \"\" --lic_server_type \"\" --lic_servers \"\"",
+            _ => ""
+        };
+        return cmdCommand;
+    }
+
+    [RelayCommand]
+    public async Task Run()
+    {
+        //TODO dont overwrite if already text in it
+        if (string.IsNullOrWhiteSpace(SelectedProduct.Key))
+        {
+            var dialogSettings = new DialogSettings()
+            {
+                Title = "No product selected",
+                Message = $"No product was selected. Make sure you have selected one product. ",
+                Color = Color.FromArgb(255, 234, 93, 97),
+                Symbol = ((char)0xEA39).ToString(),
+            };
+            await _messageDialogService.ShowDialog(dialogSettings);
+        }
+
+        Result = SetCmdCommand();
+
+
+
     }
 }
