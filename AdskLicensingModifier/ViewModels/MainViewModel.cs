47d564b5620a027dd1b3490245aaf2eb7b0af650
Tobias Wiesendanger <tobias.wiesendanger@gmail.com>
2023-03-12 01:32:19 +0100

    feat: added contetn to mainpage

--- AdskLicensingModifier/ViewModels/MainViewModel.cs
+++ AdskLicensingModifier/ViewModels/MainViewModel.cs
@@ -1,8 +1,29 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using AdskLicensingModifier.Contracts.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AdskLicensingModifier.ViewModels;
 
-public class MainViewModel : ObservableRecipient
+public partial class MainViewModel : ObservableObject
 {
+    private readonly INavigationService _navigationService;
+
+    public MainViewModel(INavigationService navigationService)
+    {
+        _navigationService = navigationService;
+    }
+
+    [RelayCommand]
+    public async Task OpenDocumentation()
+    {
+        var uri = new Uri("https://github.com/TWiesendanger/AdskLicensingModifierWinUi3");
+        await Launcher.LaunchUriAsync(uri);
+    }
+
+    [RelayCommand]
+    public void MoveToSettings()
+    {
+        _navigationService.NavigateTo(typeof(SettingsViewModel).FullName!);
+    }
 
 }
