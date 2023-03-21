08b055fd1949d7224cd2720e09df61068288afdb
Tobias Wiesendanger <tobias.wiesendanger@mum.ch>
2023-03-21 16:38:48 +0100

    fix: readded resetting of content to null at the beginning fix: changed app title

--- AdskLicensingModifier/MainWindow.xaml.cs
+++ AdskLicensingModifier/MainWindow.xaml.cs
@@ -9,6 +9,11 @@ public sealed partial class MainWindow : WindowEx
 {
     public MainWindow()
     {
+
         InitializeComponent();
+        Content = null;
+
+        Title = "AppDisplayName".GetLocalized();
+
     }
 }
