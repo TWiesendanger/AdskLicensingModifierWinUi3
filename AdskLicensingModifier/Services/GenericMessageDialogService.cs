32a64d72da28ae576b3ffcc403ae512a464121aa
Tobias Wiesendanger <tobias.wiesendanger@gmail.com>
2023-04-15 19:36:44 +0200

    fix: added app theme to fix showing contendialog in correct theme

--- AdskLicensingModifier/Services/GenericMessageDialogService.cs
+++ AdskLicensingModifier/Services/GenericMessageDialogService.cs
@@ -7,8 +7,30 @@ namespace AdskLicensingModifier.Services;
 
 public class GenericMessageDialogService : IGenericMessageDialogService
 {
+    private readonly ILocalSettingsService _localSettingsService;
+    private ElementTheme appTheme;
+    public Task Initialization
+    {
+        get;
+    }
+    public GenericMessageDialogService(ILocalSettingsService localSettingsService)
+    {
+        _localSettingsService = localSettingsService;
+
+        Initialization = InitializeAsync();
+    }
+
+    public async Task InitializeAsync()
+    {
+        var appBackground = await _localSettingsService.ReadSettingAsync<string>("AppBackgroundRequestedTheme");
+        appTheme = appBackground == "Dark" ? ElementTheme.Dark : ElementTheme.Light;
+    }
+
     public async Task ShowDialog(DialogSettings dialogSettings)
     {
+        var appBackground = await _localSettingsService.ReadSettingAsync<string>("AppBackgroundRequestedTheme");
+        appTheme = appBackground == "Dark" ? ElementTheme.Dark : ElementTheme.Light;
+
         var mainXamlRoot = App.MainWindow.Content.XamlRoot;
 
         var messageDialog = new ContentDialog()
@@ -22,6 +44,7 @@ public class GenericMessageDialogService : IGenericMessageDialogService
             IsSecondaryButtonEnabled = true,
             SecondaryButtonCommand = dialogSettings.SecondaryButtonCommand,
             Content = new MessageDialog(dialogSettings.Message, dialogSettings.Color, dialogSettings.Symbol),
+            RequestedTheme = appTheme
         };
         await messageDialog.ShowAsync();
     }
